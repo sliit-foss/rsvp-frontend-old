b6932bf85180b34b59c041af787071461c990b12
Bawantha Thilan <38850236+Bawanthathilan@users.noreply.github.com>
2021-02-07 13:14:39 -0500

    Feature/setup (#6)
    * add boostrap CDN
    
    * add scss
    
    * add upcoming card component and image slider
    
    * add upcoming card component and image slider

--- src/modules/HomeModules/HomeHero.tsx
+++ src/modules/HomeModules/HomeHero.tsx
@@ -1,7 +1,77 @@
+import "../../scss/hero.scss";
 function HomeHero() {
   return (
     <>
-      <h1>Home Hero</h1>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide p-5"
+        data-bs-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={0}
+            className="active"
+          />
+          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to={1} />
+          <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to={2} />
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src="https://dummyimage.com/600x150/007aeb/4196e5"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://dummyimage.com/600x150/007aeb/4196e5"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://dummyimage.com/600x150/007aeb/4196e5"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </div>
+          </div>
+        </div>
+        <a
+          className="carousel-control-prev"
+          href="#carouselExampleCaptions"
+          role="button"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselExampleCaptions"
+          role="button"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </a>
+      </div>
     </>
   );
 }
---
+++ src/modules/HomeModules/UpComingEvents/UpComingEventsComponent.tsx
@@ -0,0 +1,94 @@
+import "./UpComing.scss";
+import EventCard from "../EventCardComponent/EventCardComponent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
+function UpComingEvents() {
+  return (
+    <>
+      <div className="container-fluid card-container">
+        <div className="row d-flex justify-content-center p-5 ">
+          <h2 className="text-bold text-right mb-5 upcoming-text">
+            Upcoming Events{" "}
+            <span>
+              <FontAwesomeIcon icon={faArrowCircleRight} />
+            </span>
+          </h2>
+
+          <div className="col-12">
+            <div id="carousel" className="carousel slide" data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li
+                  data-target="#carousel"
+                  data-slide-to={0}
+                  className="active bg-secondary"
+                />
+                <li
+                  data-target="#carousel"
+                  data-slide-to={1}
+                  className="bg-secondary"
+                />
+                <li
+                  data-target="#carousel"
+                  data-slide-to={2}
+                  className="bg-secondary"
+                />
+              </ol>
+              <div className="carousel-inner ">
+                <div className="carousel-item active">
+                  <div className="row">
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <div className="row">
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <div className="row">
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                    <div className="col-12 col-md-6 col-xl-3 mb-4">
+                      <EventCard />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UpComingEvents;
