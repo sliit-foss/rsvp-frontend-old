d1e89920f931b45fedfe4a5622d66e2bf1042459
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-02-07 21:30:22 +0530

    add upcoming card component and image slider

--- src/components/Common/Navbar/Navbar.tsx
+++ src/components/Common/Navbar/Navbar.tsx
@@ -1,5 +1,74 @@
+import "../../../../src/scss/navbar.scss";
+
 function Navbar() {
-  return <></>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid col-11">
+          <a className="navbar-brand" href="#">
+            SLIIT RSVP PORTAL
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Events
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Clubs
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Clubs
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,13 @@
 import HomeHero from "../../modules/HomeModules/HomeHero";
+import Upcoming from "../../modules/HomeModules/upComingEvents/upComingEvents";
+import Partners from "../../modules/HomeModules/PartnersComponent/Partner";
 
 function HomePage() {
   return (
     <>
+      <Upcoming />
       <HomeHero />
+      <Partners />
     </>
   );
 }
