68a2f60cccb71d4a41d48e08fb94611457b76929
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-02-08 14:39:42 +0530

    added section

---
+++ src/modules/HomeModules/MakeGoodThings/MakeGoodThngs.tsx
@@ -0,0 +1,47 @@
+function GoodThings() {
+  return (
+    <>
+      <section className="fdb-block mt-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-12 col-md-6 mb-4 mb-md-0">
+              <img
+                alt="image"
+                className="img-fluid"
+                src="https://raw.githubusercontent.com/froala/design-blocks/000427352ad89da7fb4325037c116a3b06745608/src/imgs/draws/smile.svg"
+              />
+            </div>
+            <div className="col-12 col-md-6 col-lg-5 ml-md-auto text-left">
+              <h1> Make good things together.</h1>
+              <p className="lead">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Architecto reprehenderit voluptates nihil id harum molestias,
+                perferendis minus voluptatem ex autem enim facilis voluptatibus
+                excepturi inventore consequatur animi ipsa sed veniam.
+              </p>
+              <p>
+                <span className="badge rounded-pill bg-secondary">
+                  #SLIITFOSS
+                </span>{" "}
+                <span className="badge rounded-pill bg-secondary">
+                  #SLIITFSCS
+                </span>{" "}
+                <span className="badge rounded-pill bg-secondary">
+                  #SLIITFCMU
+                </span>{" "}
+                <span className="badge rounded-pill bg-secondary">
+                  #MOZILLASLIIT
+                </span>
+                <span className="badge rounded-pill bg-secondary">
+                  #MSCLUBOFSLIIT
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default GoodThings;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,13 @@ import HomeHero from "../../modules/HomeModules/HomeHero";
 import Upcoming from "../../modules/HomeModules/UpComingEvents/UpComingEventsComponent";
 import Partners from "../../modules/HomeModules/PartnersComponent/Partner";
 import WhatWeDo from "../../modules/HomeModules/WhatWeDoComponent/WhatWeDo";
+import MakeGoodThings from "../../modules/HomeModules/MakeGoodThings/MakeGoodThngs";
 
 function HomePage() {
   return (
     <>
       <Upcoming />
+      <MakeGoodThings />
       <HomeHero />
       <WhatWeDo />
       <Partners />
