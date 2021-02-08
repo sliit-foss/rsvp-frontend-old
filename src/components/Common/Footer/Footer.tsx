ed70ccdfd1c9130ef110f3b1016abcc6b0732bc3
Bawantha Thilan <38850236+Bawanthathilan@users.noreply.github.com>
2021-02-08 14:03:11 +0530

    Feature/home page design (#9)
    * add boostrap CDN
    
    * added what we do section
    
    * added what we do section icons
    
    * added our partner section
    
    * added Footer

--- src/components/Common/Footer/Footer.tsx
+++ src/components/Common/Footer/Footer.tsx
@@ -1,5 +1,129 @@
+import "./Footer.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import Foss from "../../../assets/images/logos/foss.png";
 function Footer() {
-  return <>Footer</>;
+  return (
+    <>
+      <footer className="footer-section mt-5">
+        <div className="container">
+          <div className="footer-content pt-5 pb-5">
+            <div className="row">
+              <div className="col-xl-4 col-lg-4 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-logo">
+                    <a href="#l">
+                      <img src={Foss} className="img-fluid" alt="logo" />
+                    </a>
+                  </div>
+                  <div className="footer-text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consec tetur adipisicing elit,
+                      sed do eiusmod tempor incididuntut consec tetur
+                      adipisicing elit,Lorem ipsum dolor sit amet.
+                    </p>
+                  </div>
+                  <div className="footer-social-icon">
+                    <span>Follow us</span>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div className="footer-widget">
+                  <div className="footer-widget-heading">
+                    <h3>Useful Links</h3>
+                  </div>
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">about</a>
+                    </li>
+                    <li>
+                      <a href="#">services</a>
+                    </li>
+                    <li>
+                      <a href="#">portfolio</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact</a>
+                    </li>
+                    <li>
+                      <a href="#">About us</a>
+                    </li>
+                    <li>
+                      <a href="#">Our Services</a>
+                    </li>
+                    <li>
+                      <a href="#">Expert Team</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact us</a>
+                    </li>
+                    <li>
+                      <a href="#">Latest News</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                <div className="footer-widget">
+                  <div className="footer-widget-heading">
+                    <h3>Subscribe</h3>
+                  </div>
+                  <div className="footer-text mb-25">
+                    <p>
+                      Don’t miss to subscribe to our new feeds, kindly fill the
+                      form below.
+                    </p>
+                  </div>
+                  <div className="subscribe-form">
+                    <form action="#">
+                      <input type="text" placeholder="Email Address" />
+                      <button>
+                        <i className="fab fa-telegram-plane" />
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="copyright-area">
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-6 col-lg-6 text-center ">
+                <div className="copyright-text ">
+                  <p>
+                    Design and Developed by <a href="#">SLIIT FOSS</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
