96caef986ca71a2154fcbea318839470610b6764
Bawantha Thilan <38850236+Bawanthathilan@users.noreply.github.com>
2021-02-08 22:53:44 +0530

    Feature/home page design (#12)
    * add boostrap CDN
    
    * added what we do section
    
    * added what we do section icons
    
    * added our partner section
    
    * added Footer
    
    * added section
    
    * added contact
    
    * change upcoming events component
    
    * change upcoming events component

--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -1,3 +1,7 @@
 import HomePage from "../pages/HomePage/HomePage";
+import ContactPage from "../pages/ContactPage/ContactPage";
 
-export const routes = [{ path: "", component: HomePage }];
+export const routes = [
+  { path: "", component: HomePage },
+  { path: "/contact", component: ContactPage },
+];
