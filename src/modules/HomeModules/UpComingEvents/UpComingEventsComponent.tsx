5c559525e751ad31b8ac3567e63eb27fb55c834e
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-02-08 13:06:25 +0530

    added what we do section

--- src/modules/HomeModules/UpComingEvents/UpComingEventsComponent.tsx
+++ src/modules/HomeModules/UpComingEvents/UpComingEventsComponent.tsx
@@ -5,7 +5,7 @@ import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 function UpComingEvents() {
   return (
     <>
-      <div className="container-fluid card-container">
+      <div className="container-fluid card-container mt-5">
         <div className="row d-flex justify-content-center p-5 ">
           <h2 className="text-bold text-right mb-5 upcoming-text">
             Upcoming Events{" "}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import HomeHero from "../../modules/HomeModules/HomeHero";
 import Upcoming from "../../modules/HomeModules/UpComingEvents/UpComingEventsComponent";
 import Partners from "../../modules/HomeModules/PartnersComponent/Partner";
+import WhatWeDo from "../../modules/HomeModules/WhatWeDoComponent/WhatWeDo";
 
 function HomePage() {
   return (
     <>
       <Upcoming />
       <HomeHero />
+      <WhatWeDo />
       <Partners />
     </>
   );
