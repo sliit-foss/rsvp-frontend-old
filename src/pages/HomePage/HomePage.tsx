e2931c0a63bcc8a9e19b73434e1ff25e370cc3eb
Bawantha Thilan <38850236+Bawanthathilan@users.noreply.github.com>
2021-02-08 14:44:04 +0530

    Feature/home page design (#10)
    * add boostrap CDN
    
    * added what we do section
    
    * added what we do section icons
    
    * added our partner section
    
    * added Footer
    
    * added section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,10 +3,14 @@ import Upcoming from "../../modules/HomeModules/UpComingEvents/UpComingEventsCom
 import Partners from "../../modules/HomeModules/PartnersComponent/Partner";
 import WhatWeDo from "../../modules/HomeModules/WhatWeDoComponent/WhatWeDo";
 
+import MakeGoodThings from "../../modules/HomeModules/MakeGoodThings/MakeGoodThngs";
+
+
 function HomePage() {
   return (
     <>
       <Upcoming />
+      <MakeGoodThings />
       <HomeHero />
       <WhatWeDo />
       <Partners />
