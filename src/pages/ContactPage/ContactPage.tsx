24874217536035103a9ec352eda6394d67aba582
Bawantha Thilan <38850236+Bawanthathilan@users.noreply.github.com>
2021-02-16 11:36:35 +0530

    Feature/contact page (#22)
    * add boostrap CDN
    
    * contact page added

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,140 +1,80 @@
 function ContactPage() {
   return (
     <>
-      <div className="row-100" />
-      <div className="row text-left">
-        <div className="col-8">
-          <h1>Contact Us</h1>
-          <p className="lead">
-            Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts.
-          </p>
-        </div>
-      </div>
-      <div className="row-100" />
-      <div className="container bg-r">
-        <div className="row-100" />
-        <div className="row">
-          <div className="col-12 col-md-6 col-lg-5">
-            <h2>Call or email</h2>
-            <p className="text-large">
-              Support, Sales, and Account Management services are currently
-              available in English
-            </p>
-            <p className="h3 mt-4 mt-lg-5">
-              <strong>Support</strong>
-            </p>
-            <p>+800 3005 4300</p>
-            <p>
-              <a href="https://www.froala.com">Contact Support</a>
-            </p>
-            <p>
-              Our technical support is available by phone or email from 11am to
-              11pm BST, Monday through Friday.
-            </p>
-            <p className="h3 mt-4 mt-lg-5">
-              <strong>Sales</strong>
-            </p>
-            <p>+800 3005 4300</p>
-            <p>
-              <a href="https://www.froala.com">Contact Sales</a>
-            </p>
-            <p>
-              Our technical support is available by phone or email from 11am to
-              11pm BST, Monday through Friday.
-            </p>
-            <p className="h3 mt-4 mt-lg-5">
-              <strong>General inquiries</strong>
-            </p>
-            <p>
-              <a href="https://www.froala.com">hello@website.com</a>
-            </p>
-          </div>
-          <div className="col-12 col-md-6 ml-auto">
-            <h2>Drop us a line</h2>
-            <form>
-              <div className="row">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="First name"
-                  />
+      <section className="fdb-block py-0">
+        <div
+          className="container py-5 my-5"
+          style={{
+            backgroundImage:
+              "url(https://raw.githubusercontent.com/froala/design-blocks/000427352ad89da7fb4325037c116a3b06745608/src/imgs/shapes/9.svg)",
+          }}
+        >
+          <div className="row py-5">
+            <div className="col py-5">
+              <div className="fdb-box fdb-touch">
+                <div className="row text-center justify-content-center">
+                  <div className="col-12 col-md-9 col-lg-7">
+                    <h1>Contact Us</h1>
+                    <p className="lead">
+                      Pityful a rethoric question ran over her cheek, then she
+                      continued her way. On her way she met a copy.
+                    </p>
+                  </div>
                 </div>
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Last name"
-                  />
-                </div>
-              </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Company Name"
-                  />
+                <div className="row justify-content-center pt-4">
+                  <div className="col-12 col-md-8">
+                    <form>
+                      <div className="row">
+                        <div className="col-12 col-md">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Name"
+                          />
+                        </div>
+                        <div className="col-12 col-md mt-4 mt-md-0">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Email"
+                          />
+                        </div>
+                      </div>
+                      <div className="row mt-4">
+                        <div className="col">
+                          <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Subject"
+                          />
+                        </div>
+                      </div>
+                      <div className="row mt-4">
+                        <div className="col">
+                          <textarea
+                            className="form-control"
+                            name="message"
+                            rows={3}
+                            placeholder="How can we help?"
+                            defaultValue={""}
+                          />
+                        </div>
+                      </div>
+                      <div className="row mt-4">
+                        <div className="col text-center">
+                          <button type="submit" className="btn-1 btn-primary">
+                            Submit
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email"
-                  />
-                </div>
-              </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Phone"
-                  />
-                </div>
-                <div className="col">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Country"
-                  />
-                </div>
-              </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <select className="form-control" required>
-                    <option>Select Department</option>
-                    <option value={1}>Support</option>
-                    <option value={2}>Sales</option>
-                    <option value={3}>Accounting</option>
-                  </select>
-                </div>
-              </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <textarea
-                    className="form-control"
-                    name="message"
-                    rows={5}
-                    placeholder="How can we help?"
-                    defaultValue={""}
-                  />
-                </div>
-              </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <button type="submit" className="btn btn-primary">
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
