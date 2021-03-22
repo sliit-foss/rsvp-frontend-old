18ed1c07405200a6296c42b73310d0f61edd4afd
Layan <layanjayasinghe2@gmail.com>
2021-03-23 00:50:50 +0530

    WIP - Homepage responsive issue

--- src/modules/HomeModules/UpComingEvents/UpComingEventsComponent.tsx
+++ src/modules/HomeModules/UpComingEvents/UpComingEventsComponent.tsx
@@ -5,44 +5,44 @@ function UpComingEvents() {
   return (
     <>
       <div className="container-fluid card-container mt-5">
-        <div className="row d-flex justify-content-center p-5 ">
-          <h2 className="text-bold text-right mb-5 upcoming-text">
+        <div className="container-fluid d-flex flex-column justify-content-center px-1">
+          <h2 className="text-bold text-right mt-5 mb-5 upcoming-text">
             Upcoming Events{" "}
             <span>
               <FontAwesomeIcon icon={faArrowCircleRight} />
             </span>
           </h2>
 
-          <div className="col-12">
-            <div className="courses-container">
-              <div className="course">
-                <div className="course-preview">
-                  <h6>Programing</h6>
-                  <h2>JavaScript Fundamentals</h2>
-                  <a href="#">
-                    <span className="badge rounded-pill text-dark bg-light">
-                      #SLIITFOSS
-                    </span>{" "}
-                    <span className="badge rounded-pill text-dark bg-light">
-                      #JAVASCRIPT
-                    </span>
-                  </a>
+          <div className="courses-container mx-auto mb-4">
+            <div className="course">
+              <div className="course-preview">
+                <h6>Programing</h6>
+                <h2>JavaScript Fundamentals</h2>
+                <a href="#">
+                  <span className="badge rounded-pill text-dark bg-light">
+                    #SLIITFOSS
+                  </span>{" "}
+                  <span className="badge rounded-pill text-dark bg-light">
+                    #JAVASCRIPT
+                  </span>
+                </a>
+              </div>
+              <div className="course-info">
+                <div className="progress-container">
+                  <div className="progress" />
+                  <span className="progress-text">6/9 Challenges</span>
                 </div>
-                <div className="course-info">
-                  <div className="progress-container">
-                    <div className="progress" />
-                    <span className="progress-text">6/9 Challenges</span>
-                  </div>
-                  <h6>Chapter 4</h6>
-                  <h2>Callbacks &amp; Closures</h2>
-                  <h6>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Deleniti totam tempore earum sint sed ipsam iste accusantium
-                    porro? Reprehenderit atque dolor culpa dicta nam quam
-                    asperiores numquam natus voluptatibus porro!
-                  </h6>
-                  <hr />
-                  <h4>By SLIIT FOSS Community</h4>
+                <h6>Chapter 4</h6>
+                <h2>Callbacks &amp; Closures</h2>
+                <h6>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Deleniti totam tempore earum sint sed ipsam iste accusantium
+                  porro? Reprehenderit atque dolor culpa dicta nam quam
+                  asperiores numquam natus voluptatibus porro!
+                </h6>
+                <hr />
+                <div className="d-flex justify-content-between align-items-center">
+                  <h5>By SLIIT FOSS Community</h5>
                   <button className="btn">RSVP</button>
                 </div>
               </div>
