83d584a90565a2660a335c3a0d3ee2ffc920b3b8
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-05-30 14:08:05 +0530

    About menu added

--- src/components/Common/Navbar/Navbar.tsx
+++ src/components/Common/Navbar/Navbar.tsx
@@ -37,6 +37,11 @@ function Navbar() {
                 </Link>
               </li>
               <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item ">
                 <Link className="nav-link" to="/contact">
                   Contact
                 </Link>
