43871d7e1560ca04faf9cbe94b52dbcd6f602ccc
susithrupasinghe <susith03program@gmail.com>
2021-06-18 04:06:38 +0530

    Ticket route cleaned up

--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -2,7 +2,6 @@ import HomePage from "../pages/HomePage/HomePage";
 import EventsPage from "../pages/EventsPage/EventsPage";
 import ContactPage from "../pages/ContactPage/ContactPage";
 import ClubsPage from "../pages/ClubsPage/ClubsPage";
-import Ticket from "../modules/TicketModules/TicketComponent/TicketComponent";
 
 export const routes = [
   { path: "/", component: HomePage },
